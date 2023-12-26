d20d1e6033f8696571326030d3ce5130945a8a93
Pavel <pavelpermakov955@gmail.com>
2023-12-26 20:44:53 +0600

    final version

--- CourseWork.Tests/UserWorkerTests.cs
+++ CourseWork.Tests/UserWorkerTests.cs
@@ -15,5 +15,16 @@ namespace CourseWork.Tests
             //Assert
             Assert.Equal("3x-56y134", correctLine);
         }
+        [Fact]
+        public void CorrectLine_CallLeadToSample_SuccessTest()
+        {
+            //Arrange
+            string incorrectLine = "3x-56y=134";
+            //Act
+            string correctLine = UserWorker.LeadToSample(incorrectLine);
+            //Assert
+            Assert.Equal("3x-56y134", correctLine);
+        }
     }
+
 }
